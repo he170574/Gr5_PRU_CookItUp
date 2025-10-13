28b7668e16b1f3822fe3a735792b966cbc0313bb
he170574 <phonghthe170574@fpt.edu.vn>
2025-10-13 21:29:51 +0700

    cm

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+
+    public GameObject optionMenuUI;
+
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+        optionMenuUI.SetActive(false);
+    }
+
+    public void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+        optionMenuUI.SetActive(false);
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenuScene"); // Đổi tên Scene menu nếu cần
+        optionMenuUI.SetActive(false);
+    }
+
+    public void LoadSceneOption()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.captureDeltaTime = 0f;
+        optionMenuUI.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/KitchenObjectListSO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[CreateAssetMenu()]
+public class KitchenObjectListSO : ScriptableObject
+{
+
+
+    public List<KitchenObjectSO> kitchenObjectSOList;
+
+
+}
