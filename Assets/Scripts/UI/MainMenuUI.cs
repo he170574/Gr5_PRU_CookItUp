630f568924634d6fe102de161922dd3d18a82038
he170574 <phonghthe170574@fpt.edu.vn>
2025-11-06 12:05:34 +0700

    cm
    update about us

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -6,22 +6,29 @@ using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour
 {
-
-
     [SerializeField] private Button playButton;
     [SerializeField] private Button quitButton;
-
-
+    [SerializeField] private Button aboutUsButton;
+    [SerializeField] private Button backButton;
     private void Awake()
     {
-        playButton.onClick.AddListener(() => {
-            Loader.Load(Loader.Scene.LobbyScene);
-        });
-        quitButton.onClick.AddListener(() => {
-            Application.Quit();
-        });
+        if (playButton != null)
+            playButton.onClick.AddListener(() => { LoadScene("LobbyScene"); });
+
+        if (quitButton != null)
+            quitButton.onClick.AddListener(() => { Application.Quit(); });
+
+        if (aboutUsButton != null)
+            aboutUsButton.onClick.AddListener(() => { LoadScene("AboutUsScene"); });
+
+        if (backButton != null)
+            backButton.onClick.AddListener(() => { LoadScene("MainMenuScene"); });
 
         Time.timeScale = 1f;
     }
 
+    private void LoadScene(string AboutUsScene)
+    {
+        SceneManager.LoadScene(AboutUsScene);
+    }
 }
